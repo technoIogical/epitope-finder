bf737a95de5653eadbf54b35402b2e8656ff5016
tecnologcal <rodin2009@gmail.com>
2025-04-30 16:49:26 -0700

    proper jsonl
    name fixes

--- savefiles.py
+++ savefiles.py
@@ -9,8 +9,10 @@ def print_to_file(extracted_data, target):
     if not os.path.exists(storage_path):
         os.makedirs(storage_path)
 
-    output_file_path = os.path.join(storage_path, f"epitope_output_{target}.json")
-    with open(output_file_path, "w") as f:
-        for record in extracted_data:
-            json.dump(record, f, ensure_ascii=False)
-            f.write("\n")
+    output_file_path = os.path.join(
+        storage_path, f"epitope_output{target}.json"
+    )  # Note: You can still use .json extension
+    with open(output_file_path, "w", encoding="utf-8") as f:  # Ensure UTF-8 encoding
+        for record in extracted_data:  # Iterate through the list of dictionaries
+            json.dump(record, f, ensure_ascii=False)  # Write each dictionary as JSON
+            f.write("\n")  # Add a newline character
