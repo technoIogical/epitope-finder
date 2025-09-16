1df3fd3414129d86e5216d89286a589656eb62dd
tecnologcal <rodin2009@gmail.com>
2025-09-16 12:19:49 -0700

    backend functionality added

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,59 @@
 import functions_framework
+import os
+
 
 @functions_framework.http
-def hello_http(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
-    """
-    request_json = request.get_json(silent=True)
-    request_args = request.args
+def fetch_bq_epitopes(request):
+    if request.method == "OPTIONS":
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Max-Age": "3600",
+        }
 
-    if request_json and 'name' in request_json:
-        name = request_json['name']
-    elif request_args and 'name' in request_args:
-        name = request_args['name']
-    else:
-        name = 'World'
-    return 'Hello {}!'.format(name)
+        return ("", 204, headers)
+
+    # Set CORS headers for the main request
+    headers = {"Access-Control-Allow-Origin": "*"}
+
+    request_json = request.get_json(silent=true)
+
+    if not request_json or "input_alleles" not in request_json:
+        return ("Bad Request: `input_alleles` array is required.", 400, headers)
+
+    input_alleles = request_json["input_alleles"]
+
+    project_id = os.environ.get("EpitopeFinder", "epitopefinder-458404")
+    dataset_id = "epitopefinder-458404.epitopes"
+    query_name = "output_logic"
+
+    try:
+        client = bigquery.Client(project=project_id)
+
+        query_job_config = bigquery.QueryJobConfig(
+            query=f"#bq:jobs:query:{project_id}.{dataset_id}.{query_name}",
+            query_parameters=[
+                bigquery.ArrayQueryParameter(
+                    "patient_alleles", "STRING", patient_alleles
+                ),
+            ],
+        )
+
+        query_job = client.query(
+            f"SELECT * FROM `{project_id}.{dataset_id}.{query_name}`",
+            job_config=query_job_config,
+        )
+        rows = query_job.result()
+
+        results = [dict(row) for row in rows]
+
+        return (results, 200, headers)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return (f"An error occurred: {str(e)}", 500, headers)
+
+    return ("Hello World!", 200, headers)
