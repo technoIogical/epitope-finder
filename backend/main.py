3ee582b83d97608b468a46d260d2f76a4ad86f00
tecnologcal <rodin2009@gmail.com>
2025-09-16 12:57:30 -0700

    naming issue fixed

--- backend/main.py
+++ backend/main.py
@@ -6,18 +6,14 @@ import os
 @functions_framework.http
 def fetch_bq_epitopes(request):
     if request.method == "OPTIONS":
-        # Allows GET requests from any origin with the Content-Type
-        # header and caches preflight response for an 3600s
         headers = {
             "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET",
+            "Access-Control-Allow-Methods": "POST",
             "Access-Control-Allow-Headers": "Content-Type",
             "Access-Control-Max-Age": "3600",
         }
-
         return ("", 204, headers)
 
-    # Set CORS headers for the main request
     headers = {"Access-Control-Allow-Origin": "*"}
 
     request_json = request.get_json(silent=True)
@@ -35,7 +31,6 @@ def fetch_bq_epitopes(request):
         client = bigquery.Client(project=project_id)
 
         query_job_config = bigquery.QueryJobConfig(
-            query=f"#bq:jobs:query:{project_id}.{dataset_id}.{query_name}",
             query_parameters=[
                 bigquery.ArrayQueryParameter("input_alleles", "STRING", input_alleles),
             ],
